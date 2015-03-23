515e90be563f3b7cbf620663971f7e9128ffe773
David G Chaves <davidgchaves@gmail.com>
2015-03-23 13:49:58 +0100

    Add 'lint-client' and 'lint-test' gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,18 @@ var gulp       = require('gulp'),
     browserify = require('browserify'),
     phantomjs  = require('gulp-mocha-phantomjs');
 
+// Lint Tasks
+gulp.task('lint-client', function() {
+  return gulp.src('./client/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('lint-test', function() {
+  return gulp.src('./test/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
 gulp.task('default', function() {});
 
