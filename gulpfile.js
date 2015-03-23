f8635bb271a09eb7ee5a599413a308afc9dbcb75
David G Chaves <davidgchaves@gmail.com>
2015-03-23 12:15:41 +0100

    Add gulp-rename and gulp-jshint as devDependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp       = require('gulp'),
+    rename     = require('gulp-rename'),
+    jshint     = require('gulp-jshint'),
     browserify = require('browserify'),
     phantomjs  = require('gulp-mocha-phantomjs');
 
